f7e7465b648aacb1e1e92c9d3babed02f9f90979
LL <1158054401@qq.com>
2020-01-10 09:17:34 +0800

    init app

---
+++ build/precache-manifest.796177007e571c93841929cc31888dfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc341baf5c5b5581521213f4451794f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18973209c4c2a4c6fa81",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9cbc0492a6ec3c68328e",
+    "url": "/static/js/2.fbbc0341.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fbbc0341.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18973209c4c2a4c6fa81",
+    "url": "/static/js/main.9f2e56ce.chunk.js"
+  },
+  {
+    "revision": "e7608a1c3e3c87040c8e",
+    "url": "/static/js/runtime-main.177d723e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
